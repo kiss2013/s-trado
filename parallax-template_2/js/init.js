8c003a5c991547443ef31cd508eee025ab449e90
apython <c131mai@gmail.com>
2017-07-30 18:10:15 +0300

    I am tired...

---
+++ parallax-template_2/js/init.js
@@ -0,0 +1,12 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+     // Plugin initialization
+     $('.modal').modal();
+     $('.materialboxed').materialbox();
+     $('ul.tabs').tabs();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
